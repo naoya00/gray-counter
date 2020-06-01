9588ade4b6717fa94a1c62adc6f55f1d294cd5e9
naoya00 <bsk470@gmail.com>
2020-06-01 15:35:16 +0900

    add components

---
+++ src/components/Sticker.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="item">
+    <h2>{{ title }}</h2>
+    <p>
+      <span class="value">{{ value }}</span>
+      <span class="unit">[ {{ unit }} ]</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+    title: {
+      type: String
+    },
+    value: {
+      type: Number,
+      default: 0
+    },
+    unit: {
+      type: String
+    }
+  }
+}
+</script>
