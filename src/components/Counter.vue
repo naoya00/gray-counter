158fde2a3b5fd7c3f199f3a6e9edef5ea446bc23
naoya00 <bsk470@gmail.com>
2020-06-01 15:32:05 +0900

    couter vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="item">
+    <Sticker
+      :title="title"
+      :unit="unit"
+      :value="value"
+    />
+    <button class="add-button"
+      @click="plusOne">追加</button>
+  </div>
+</template>
+
+<script>
+import Sticker from './Sticker.vue'
+export default {
+  components: {
+    Sticker
+  },
+  props: {
+    title: {
+      type: String
+    },
+    unit: {
+      type: String
+    },
+    varName: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      value: 0
+    }
+  },
+  methods: {
+    plusOne () {
+      this.value += 1
+      this.$emit('updated', this.value, this.varName)
+    }
+  }
+}
+</script>
